e0eec104c454f1d3477742dc7ec10ef83496e0e0
Juano <3674588+JuanoD@users.noreply.github.com>
2018-02-05 20:22:10 -0500

    Removed content. This file is useless until I find a way to pass an object created by the server file.

--- Modules/methods_to_mirror.py
+++ Modules/methods_to_mirror.py
@@ -1,11 +1,10 @@
 from opcua import uamethod
+from .tools import argget
 import time
+import threading  # Used for running methods on threads, so it doesn't block the app
+
+import logging  # May be used to know when a thread is running, may be deleted from this one
+
+# This file is useless. May be nice if it works as I wish, but I need some globals and I cannot use them here.
 
 
-@uamethod
-def test(parent, ActionName, MethodArguments):
-    print("My parent is: ", parent,
-          ActionName, MethodArguments)
-    time.sleep(3)
-    print("Han pasado tres segundos")
-    return ["2", "OK"]
