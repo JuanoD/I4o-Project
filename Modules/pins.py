426ffcc0895c65049405fedb0243231ea460c417
Juano <3674588+JuanoD@users.noreply.github.com>
2018-02-05 20:27:03 -0500

    Created fake wiringpi for testing. Changed some stuff in the ControlMezcladora class. It nows receive a special dictionary for init, so it can initialize the pins and perform cleanup. The class may be deleted from here, so I need some objects from the server file to use it. It was moved to the server file.

--- Modules/pins.py
+++ Modules/pins.py
@@ -1,8 +1,81 @@
 """
 This module translates the pin number from the device to WiringPi
 """
-import wiringpi2 as wiringpi
+import logging
+# import wiringpi2 as wiringpi  # I'll be using a fake wiringPi for testing
 import time
+import random
+
+ttp = { # Terminal to pin dictionary
+    'w01': 0,
+    'w02': 1,  # PWM0
+    'w03': 2,
+    'w04': 3,
+    'w05': 4,
+    'w06': 5,
+    'w07': 6,
+    'w08': 10,
+    'w09': 11,
+    'w10': 12,
+    'w11': 13,
+    'w12': 14,
+    'w13': 21,
+    'w14': 22,
+    'w15': 23,  # PWM1
+    'w16': 24,  # PWM1?
+    'w17': 25,
+    'w18': 26,  # PWM0
+    'w19': 27,
+    'w20': 28,
+    'w21': 29,
+    'w22': 30,
+    'w23': 31,
+    'w24': 'GND',
+    'w25': 'VCC',
+
+    'in_m': 0,
+    'out_m': 1,
+    'alt_m': 2,
+}
+
+mx1pins = {
+    'inputs': {'start': None, 'end': None, },
+    'outputs': {'forward': None, 'backward': None, 'run': None, 'extend': None, 'contract': None, },
+    'pwm': {'duty': None, },
+}
+
+mx2pins = {
+    'inputs': {'start': None, 'end': None, },
+    'outputs': {'forward': None, 'backward': None, 'run': None, 'extend': None, 'contract': None, },
+    'pwm': {'duty': None, },
+}
+
+
+class wiringpi(object):
+    @staticmethod
+    def wiringPiSetup():
+        logging.info('Setup for using wiringpi pin numbering')
+
+    @staticmethod
+    def digitalWrite(pin, state):
+        logging.info('Set {} to {}.'.format(pin, state))
+
+    @staticmethod
+    def digitalRead(pin):
+        read = random.choice([True, False])
+        logging.info('Read {} from pin {}.'.format(read, pin))
+        return read
+
+    @staticmethod
+    def pwmWrite(pin, duty):
+        logging.info('Set pin {} duty to {}.'.format(pin, duty))
+
+    @staticmethod
+    def pinMode(pin, mode):
+        logging.info('Set pin {} to mode {}.'.format(pin, mode))
+
+
+# Everything beyond this point may be deleted
 
 
 class P(object):
@@ -36,25 +109,11 @@ class P(object):
     out_m = 1
     alt_m = 2
 
-    @staticmethod
-    def initial_setup():
-        """
-        Here you should put your initialization for pins
-        """
-        wiringpi.wiringPiSetup()
-        wiringpi.pinMode(P.w15, P.out_m)
-
-    @staticmethod
-    def final_cleanup():
-        """
-        Here, you should perform pin cleanup
-        """
-        wiringpi.digitalWrite(P.w15, 0)
-        wiringpi.pinMode(P.w15, 0)
-
 
 class ControlMezcladora(object):
-    def __init__(self, forward, backward, extend, end, start, contract=None, duty=None, run=None):
+    def __init__(self, **kwargs: 'Dictionary containing the pins for: '
+                                 'forward, backward, extend, end, start,'
+                                 'and if not needed contract, duty, and run may be None'):
         """
         ControlMotor(forward_pin, backward_pin)
 
@@ -67,15 +126,40 @@ class ControlMezcladora(object):
         :param contract: If valve is bistable, this pin must be assigned  # This can be a light or sound if piston works
             manually.
         :param duty: If motor is PWM controllable, set this pin to the PWM output. Must be between 0 and 1023
+        :param run: This should be a manual start. Will also be manual
         """
-        self.forward_pin = forward
-        self.backward_pin = backward
-        self.extend = extend
-        self.end = end
-        self.start = start
-        self.contract = contract
-        self.duty = duty
-        self.run = run
+        # forward, backward, extend, end, start, contract=None, duty=None, run=None
+        self.inputs = kwargs['inputs']
+        self.outputs = kwargs['outputs']
+        self.pwm = kwargs['pwm']
+        self.forward_pin = self.outputs['forward']
+        self.backward_pin = self.outputs['backward']
+        self.extend = self.outputs['extend']
+        self.end = self.inputs['end']
+        self.start = self.inputs['start']
+        self.contract = self.outputs['contract']
+        self.duty = self.pwm['duty']
+        self.run = self.outputs['run']
+
+        for pin in self.inputs.values():  # Initialize inputs (I think this is not necessary
+            if pin:
+                wiringpi.pinMode(pin, ttp['in_m'])
+        for pin in self.outputs.values():  # Initialize outputs
+            if pin:
+                wiringpi.pinMode(pin, ttp['out_m'])
+        for pin in self.pwm.values():  # Initializing pwm output
+            if pin:
+                wiringpi.pinMode(pin, ttp['alt_m'])
+
+    def clear_pins(self):  # Must be called when stopping server
+        for pin in self.outputs.values():
+            if pin:
+                wiringpi.digitalWrite(pin, 0)
+                wiringpi.pinMode(pin, ttp['in_m'])
+        for pin in self.pwm.values():
+            if pin:
+                wiringpi.pwmWrite(pin, 0)
+                wiringpi.pinMode(pin, ttp['alt_m'])
 
     @staticmethod
     def cl(total_time, alternating_time):
@@ -129,7 +213,7 @@ class ControlMezcladora(object):
         wiringpi.digitalWrite(self.extend, 0)  # This contracts if piston uses monostable valve
         if self.contract:
             wiringpi.digitalWrite(self.contract, 1)  # This contracts if piston uses bistable valve
-            while wiringpi.digitalRead(self.start):
+            while not wiringpi.digitalRead(self.start):
                 # Wait for piston to reach start
                 pass
             wiringpi.digitalWrite(self.contract, 0)  # In bistable valves, this must be set to 0
